76d0e47b6a68cf3915d2ffe95d903ed58a45e18b
Jorden Hilton <jorden.hilton@gmail.com>
2021-02-01 15:44:12 -0600

    HTML CSS & JS

---
+++ landingPage/js/index.js
@@ -0,0 +1,26 @@
+// DOM Elements
+const greeting = document.getElementById('greeting');
+
+// Set Background and Greeting
+function backgroundGreet() {
+  let today = new Date(),
+    hour = today.getHours();
+
+  if (hour < 12) {
+    // Morning
+    document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
+    greeting.textContent = 'Good Morning ';
+  } else if (hour < 18) {
+    // Afternoon
+    document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+    greeting.textContent = 'Good Afternoon ';
+  } else {
+    // Evening
+    document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
+    greeting.textContent = 'Good Evening ';
+    document.body.style.color = 'white';
+  }
+}
+
+// Run
+backgroundGreet();

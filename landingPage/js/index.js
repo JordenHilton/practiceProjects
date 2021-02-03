eecf03970a8026afb57c13ac0de212ac90636be1
Jorden Hilton <jorden.hilton@gmail.com>
2021-02-03 13:35:48 -0600

    HTML edits

--- landingPage/js/index.js
+++ landingPage/js/index.js
@@ -9,18 +9,19 @@ function backgroundGreet() {
   if (hour < 12) {
     // Morning
     document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
-    greeting.textContent = 'Good Morning ';
+    greeting.textContent = 'Good Morning,';
   } else if (hour < 18) {
     // Afternoon
     document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
-    greeting.textContent = 'Good Afternoon ';
+    greeting.textContent = 'Good Afternoon,';
   } else {
     // Evening
     document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
-    greeting.textContent = 'Good Evening ';
+    greeting.textContent = 'Good Evening,';
     document.body.style.color = 'white';
   }
 }
 
 // Run
 backgroundGreet();
+
